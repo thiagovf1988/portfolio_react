94da84be50b86b7382d8e87e9d0b4de7383fe58b
thiagovf1988 <61379143+thiagovf1988@users.noreply.github.com>
2020-05-22 12:51:47 +0100

    f

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,17 +2,17 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: 'ssssss', // e.g: 'Name | Developer'
-  lang: '6hty', // e.g: en, es, fr, jp
-  description: 'erw', // e.g: Welcome to my website
+  title: 'Thiago Fernandes | Portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: 'ss',
-  name: 'dd',
-  subtitle: '33',
-  cta: '222',
+  title: 'Hello. My name is',
+  name: 'Thiago Vieira Fernandes',
+  subtitle: 'I am the Developer of this website.',
+  cta: 'Get to know my previous work',
 };
 
 // ABOUT DATA
@@ -57,9 +57,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: 'tjhnw45',
-  btn: '4h55',
-  email: 'h5h5',
+  cta: 'Below you find my e-mail address and my social media pages',
+  btn: 'E-mail',
+  email: 'thiagovf.eng@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +68,27 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/ThiagoF42713207',
     },
     {
       id: uuidv1(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/thiagovf1988',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/thiago-vieira-fernandes-6073b130/',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/thiagovf1988',
     },
   ],
 };
 
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
-};
+// // Github start/fork buttons
+// export const githubButtons = {
+//   isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+// };
