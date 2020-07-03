27e57a946e433dc5d6d037baf80da16c804d8f12
Thiago Vieira Fernandes <thiagovf.eng@gmail.com>
2020-07-03 19:47:01 +0100

    update

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: 'Thiago Fernandes | Portfolio', // e.g: 'Name | Developer'
+  title: 'Thiago Fernandes | Portfolio', // e.g: 'Name | Developer';
   lang: 'en', // e.g: en, es, fr, jps
   description: 'Welcome to my website', // e.g: Welcome to my website
 };
@@ -19,9 +19,11 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: 'My name is Thiago. I am a Full stack developer with a engineer degree.',
-  paragraphTwo: 'My recent jobs were focused on engineer projects for major companies in the UK. I have worked coding since college with different languages and purposes. Check below some of my recent websites. ',
+  paragraphTwo:
+    'My recent jobs were focused on engineer projects for major companies in the UK. I have worked coding since college with different languages and purposes. Check below some of my recent websites. ',
   paragraphThree: 'Also, feel free to contact me anytime.',
-  resume: 'https://docs.google.com/document/d/1sSNo4wCg6s-oIVs9Z8usCRS8sZodqy3hexVdvi2f1vM/edit?usp=sharing', // if no resume, the button will not show up
+  resume:
+    'https://docs.google.com/document/d/1sSNo4wCg6s-oIVs9Z8usCRS8sZodqy3hexVdvi2f1vM/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -31,7 +33,8 @@ export const projectsData = [
     img: 'rslogo.png',
     title: 'Robot Searcher',
     info: 'A simple searcher of robots.',
-    info2: 'A React app managed through npm and deployed on Github. The code organizes the components, containers and error handling.  It uses JavaScript, HTML5, CSS3, and JSON on its script. The user interaction is achieved through a One-way data flow where the parents components feed their children with the State and get the properties from them. ',
+    info2:
+      'A React app managed through npm and deployed on Github. The code organizes the components, containers and error handling.  It uses JavaScript, HTML5, CSS3, and JSON on its script. The user interaction is achieved through a One-way data flow where the parents components feed their children with the State and get the properties from them. ',
     url: 'https://thiagovf1988.github.io/Robot-Searcher/',
     repo: 'https://github.com/thiagovf1988/Robot-Searcher', // if no repo, the button will not show up
   },
@@ -40,7 +43,8 @@ export const projectsData = [
     img: 'calogo.png',
     title: 'Algorithms',
     info: 'A website to run algorithms',
-    info2: 'This project is based on the book "Algorithms to live by" and in the website "daily coding". At the moment it just have the "Current Average" which allows the user to see the evolution of the average number of its list as he adds new numbers. The "Secretary problem", "Monte Carlo Simulation", "A/B tests", and "Bucket sort" are in prospect to be implemented as well.',
+    info2:
+      'This project is based on the book "Algorithms to live by" and in the website "daily coding". At the moment it just have the "Current Average" which allows the user to see the evolution of the average number of its list as he adds new numbers. The "Secretary problem", "Monte Carlo Simulation", "A/B tests", and "Bucket sort" are in prospect to be implemented as well.',
     url: 'https://thiagovf1988.github.io/algorithms/',
     repo: 'https://github.com/thiagovf1988/algorithms', // if no repo, the button will not show up
   },
@@ -49,7 +53,8 @@ export const projectsData = [
     img: 'cailogo.png',
     title: 'Chess AI',
     info: 'Software to face opponents in chess matches',
-    info2: 'I am still working on this project as it demands a considerable time. At the moment the page below shows the current stage of the software (makes basic moves and can see the moves from the adversary). Neural network and convolutional Q-learning are being implemented',
+    info2:
+      'I am still working on this project as it demands a considerable time. At the moment the page below shows the current stage of the software (makes basic moves and can see the moves from the adversary). Neural network and convolutional Q-learning are being implemented',
     url: 'https://thiagovf1988.github.io/chessai/',
     repo: 'https://github.com/thiagovf1988/chessai', // if no repo, the button will not show up
   },
